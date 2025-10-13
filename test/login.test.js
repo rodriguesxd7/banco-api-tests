faa1f7d3f4ad2a1a24ac9a2435e939ff44426696
Bruno Rodrigues <rodriguesxd7@hotmail.com>
2025-10-13 20:57:42 -0300

    adding mochawesome to generate graphic reports

--- test/login.test.js
+++ test/login.test.js
@@ -1,6 +1,11 @@
 //comando para subir o banco no CMD - mysql -u root -p
 //sempre subir o rest-api com npm run server
 //sempre subir o banco-web com npm run server
+//a biblioteca mocha awesome é utilizada para criar relaatorios gráficos
+//utilizar npm i mochawesome para fazer download da lib
+//adicionar o comando --reporter mochawesome ao seu comando de testes para gerar o relatório
+//no package.json adicionar scrips o seguinte comando mocha ./test/**/*.test.js --timeout=200000 --reporter mochawesome
+//caso voce esteja usando mocha para criar seus testes
 
 const request = require('supertest');
 const { expect } = require ('chai');
